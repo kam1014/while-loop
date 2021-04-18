fced28fb33d26afdca621f0bec3c6c0925c54a65
kam1014 <49304905+kam1014@users.noreply.github.com>
2021-04-18 19:52:25 -0400

    This is will: add start files

--- start/js/while.js
+++ start/js/while.js
@@ -2,3 +2,10 @@ function getRandomNumber(upper) {
   return Math.floor( Math.random() * upper ) + 1;
 }
 
+let counter = 0;
+while(counter < 10) {
+  console.log(`The random number is ${getRandomNumber(10)}` );
+  counter += 1;
+ 
+}
+
