21bf3f53a4246f4a8c955c335abc694f2f586c68
kam1014 <49304905+kam1014@users.noreply.github.com>
2021-04-18 08:40:05 -0400

    This will delete final files

--- final/js/loop-conditions.js
+++
@@ -1,7 +0,0 @@
-const main = document.querySelector('main');
-const randomNumber = getRandomNumber(10);
-let guess;
-
-function getRandomNumber(upper) {
-  return Math.floor( Math.random() * upper ) + 1;
-}
